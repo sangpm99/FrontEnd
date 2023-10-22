06c98758688c4df665b8e15ef16bbf36b2f5f7dd
sangpm99 <sangpm72@wru.vn>
2023-10-22 14:26:17 +0700

    Update Cart

---
+++ src/pages/Frame/components/Header/Navbar/components/CartIcon/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {BsHandbag} from "react-icons/bs";
+
+function Cart() {
+    return (
+        <BsHandbag className="text-2xl color-primary cursor-pointer"/>
+    );
+}
+
+export default Cart;
